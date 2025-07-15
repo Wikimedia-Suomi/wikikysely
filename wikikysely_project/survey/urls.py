e72dcc162c9c53becc4e2eb1b0b83e3aa9324cad
zache-fi <kimmo.virtanen@gmail.com>
2025-07-15 20:02:37 +0300

    Move question deletion to survey edit and add answer management

--- wikikysely_project/survey/urls.py
+++ wikikysely_project/survey/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('survey/<int:pk>/answer/', views.answer_survey, name='answer_survey'),
     path('survey/<int:survey_pk>/question/add/', views.question_add, name='question_add'),
     path('question/<int:pk>/delete/', views.question_delete, name='question_delete'),
+    path('question/<int:pk>/restore/', views.question_restore, name='question_restore'),
+    path('answer/<int:pk>/edit/', views.answer_edit, name='answer_edit'),
+    path('answer/<int:pk>/delete/', views.answer_delete, name='answer_delete'),
     path('answers/', views.answer_list, name='answer_list'),
     path('results/<int:pk>/', views.survey_results, name='survey_results'),
 ]
