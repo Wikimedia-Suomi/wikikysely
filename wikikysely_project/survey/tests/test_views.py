c1bbdd7f4c0490eda96b218d90bc81a59288e066
zache-fi <kimmo.virtanen@gmail.com>
2025-07-25 06:30:18 +0300

    Add question editing

--- wikikysely_project/survey/tests/test_views.py
+++ wikikysely_project/survey/tests/test_views.py
@@ -12,6 +12,7 @@ class SurveyFlowTests(TransactionTestCase):
     def setUpClass(cls):
         super().setUpClass()
         from django.db import connection
+
         connection.disable_constraint_checking()
         try:
             with connection.schema_editor(atomic=False) as schema_editor:
@@ -24,6 +25,7 @@ class SurveyFlowTests(TransactionTestCase):
     @classmethod
     def tearDownClass(cls):
         from django.db import connection
+
         connection.disable_constraint_checking()
         try:
             with connection.schema_editor(atomic=False) as schema_editor:
@@ -33,25 +35,26 @@ class SurveyFlowTests(TransactionTestCase):
         finally:
             connection.enable_constraint_checking()
         super().tearDownClass()
+
     def setUp(self):
-        activate('en')
+        activate("en")
         User = get_user_model()
         self.users = [
-            User.objects.create_user(username=f'tester{i}', password='pass')
+            User.objects.create_user(username=f"tester{i}", password="pass")
             for i in range(1, 4)
         ]
         self.user = self.users[0]
-        self.client.login(username=self.user.username, password='pass')
+        self.client.login(username=self.user.username, password="pass")
 
     def _create_survey(self):
         return Survey.objects.create(
-            title='Test Survey',
-            description='desc',
+            title="Test Survey",
+            description="desc",
             creator=self.user,
-            state='running',
+            state="running",
         )
 
-    def _create_question(self, survey, text='Question?'):
+    def _create_question(self, survey, text="Question?"):
         return Question.objects.create(
             survey=survey,
             text=text,
@@ -60,110 +63,144 @@ class SurveyFlowTests(TransactionTestCase):
 
     def _create_questions(self, survey, count=10):
         return [
-            self._create_question(survey, text=f'Question {i}?')
+            self._create_question(survey, text=f"Question {i}?")
             for i in range(1, count + 1)
         ]
 
     def test_survey_edit(self):
         survey = self._create_survey()
         data = {
-            'title': 'Edited Survey',
-            'description': 'changed',
-            'state': 'paused',
+            "title": "Edited Survey",
+            "description": "changed",
+            "state": "paused",
         }
-        response = self.client.post(reverse('survey:survey_edit'), data)
+        response = self.client.post(reverse("survey:survey_edit"), data)
         survey.refresh_from_db()
-        self.assertEqual(survey.title, 'Edited Survey')
-        self.assertEqual(survey.state, 'paused')
-        self.assertRedirects(response, reverse('survey:survey_detail'))
+        self.assertEqual(survey.title, "Edited Survey")
+        self.assertEqual(survey.state, "paused")
+        self.assertRedirects(response, reverse("survey:survey_detail"))
 
     def test_add_question(self):
         survey = self._create_survey()
-        data = {'text': 'What do you think?'}
-        response = self.client.post(reverse('survey:question_add'), data)
+        data = {"text": "What do you think?"}
+        response = self.client.post(reverse("survey:question_add"), data)
         self.assertEqual(survey.questions.filter(deleted=False).count(), 1)
         question = survey.questions.first()
-        self.assertEqual(question.text, 'What do you think?')
-        self.assertRedirects(response, reverse('survey:survey_detail'))
+        self.assertEqual(question.text, "What do you think?")
+        self.assertRedirects(response, reverse("survey:survey_detail"))
 
     def test_delete_and_restore_question(self):
         survey = self._create_survey()
         question = self._create_question(survey)
         # delete
-        response = self.client.post(reverse('survey:question_delete', kwargs={'pk': question.pk}))
+        response = self.client.post(
+            reverse("survey:question_delete", kwargs={"pk": question.pk})
+        )
         question.refresh_from_db()
         self.assertTrue(question.deleted)
-        self.assertRedirects(response, reverse('survey:survey_edit'))
+        self.assertRedirects(response, reverse("survey:survey_edit"))
         # restore
-        response = self.client.post(reverse('survey:question_restore', kwargs={'pk': question.pk}))
+        response = self.client.post(
+            reverse("survey:question_restore", kwargs={"pk": question.pk})
+        )
         question.refresh_from_db()
         self.assertFalse(question.deleted)
-        self.assertRedirects(response, reverse('survey:survey_edit'))
+        self.assertRedirects(response, reverse("survey:survey_edit"))
+
+    def test_edit_question(self):
+        survey = self._create_survey()
+        question = self._create_question(survey)
+        data = {"text": "Updated question?"}
+        response = self.client.post(
+            reverse("survey:question_edit", kwargs={"pk": question.pk}),
+            data,
+        )
+        question.refresh_from_db()
+        self.assertEqual(question.text, "Updated question?")
+        self.assertRedirects(response, reverse("survey:survey_edit"))
+
+    def test_cannot_edit_question_answered_by_others(self):
+        other_user = self.users[1]
+        survey = Survey.objects.create(
+            title="Test", description="d", creator=other_user, state="running"
+        )
+        question = Question.objects.create(
+            survey=survey, text="Original?", creator=self.user
+        )
+        Answer.objects.create(question=question, user=other_user, answer="yes")
+        data = {"text": "Updated?"}
+        response = self.client.post(
+            reverse("survey:question_edit", kwargs={"pk": question.pk}),
+            data,
+        )
+        question.refresh_from_db()
+        self.assertEqual(question.text, "Original?")
+        self.assertRedirects(response, reverse("survey:survey_detail"))
 
     def test_answer_question_and_edit(self):
         survey = self._create_survey()
         questions = self._create_questions(survey, 10)
 
         # answer by first user (already logged in)
-        data = {'question_id': questions[0].pk, 'answer': 'yes'}
-        response = self.client.post(reverse('survey:answer_survey'), data)
+        data = {"question_id": questions[0].pk, "answer": "yes"}
+        response = self.client.post(reverse("survey:answer_survey"), data)
         self.assertEqual(Answer.objects.count(), 1)
         answer = Answer.objects.get(question=questions[0], user=self.user)
-        self.assertEqual(answer.answer, 'yes')
+        self.assertEqual(answer.answer, "yes")
         self.assertRedirects(
             response,
-            reverse('survey:answer_survey'),
+            reverse("survey:answer_survey"),
             fetch_redirect_response=False,
         )
 
         # answer by second and third users
         for idx, user in enumerate(self.users[1:], start=1):
             self.client.logout()
-            self.client.login(username=user.username, password='pass')
-            data = {'question_id': questions[idx].pk, 'answer': 'yes'}
-            self.client.post(reverse('survey:answer_survey'), data)
+            self.client.login(username=user.username, password="pass")
+            data = {"question_id": questions[idx].pk, "answer": "yes"}
+            self.client.post(reverse("survey:answer_survey"), data)
             ans = Answer.objects.get(question=questions[idx], user=user)
-            self.assertEqual(ans.answer, 'yes')
+            self.assertEqual(ans.answer, "yes")
         self.assertEqual(Answer.objects.count(), 3)
 
         # edit first user's answer
         self.client.logout()
-        self.client.login(username=self.user.username, password='pass')
-        edit_data = {'question_id': questions[0].pk, 'answer': 'no'}
+        self.client.login(username=self.user.username, password="pass")
+        edit_data = {"question_id": questions[0].pk, "answer": "no"}
         response = self.client.post(
-            reverse('survey:answer_question', kwargs={'pk': questions[0].pk}),
+            reverse("survey:answer_question", kwargs={"pk": questions[0].pk}),
             edit_data,
         )
         answer.refresh_from_db()
-        self.assertEqual(answer.answer, 'no')
+        self.assertEqual(answer.answer, "no")
         self.assertRedirects(
             response,
-            reverse('survey:survey_detail'),
+            reverse("survey:survey_detail"),
         )
 
     def test_results_view(self):
         survey = self._create_survey()
         question = self._create_question(survey)
-        Answer.objects.create(question=question, user=self.user, answer='yes')
-        response = self.client.get(reverse('survey:survey_results'))
+        Answer.objects.create(question=question, user=self.user, answer="yes")
+        response = self.client.get(reverse("survey:survey_results"))
         self.assertEqual(response.status_code, 200)
-        data = response.context['data'][0]
-        self.assertEqual(data['yes'], 1)
-        self.assertEqual(data['no'], 0)
-        self.assertIn('published', data)
-        self.assertEqual(data['agree_ratio'], 100.0)
-        self.assertEqual(data['my_answer'], 'Yes')
-        self.assertEqual(response.context['total_users'], 1)
-        self.assertContains(response, 'Answer table')
+        data = response.context["data"][0]
+        self.assertEqual(data["yes"], 1)
+        self.assertEqual(data["no"], 0)
+        self.assertIn("published", data)
+        self.assertEqual(data["agree_ratio"], 100.0)
+        self.assertEqual(data["my_answer"], "Yes")
+        self.assertEqual(response.context["total_users"], 1)
+        self.assertContains(response, "Answer table")
 
     def test_results_view_displays_my_answer_column(self):
         survey = self._create_survey()
         question = self._create_question(survey)
-        Answer.objects.create(question=question, user=self.user, answer='yes')
-        response = self.client.get(reverse('survey:survey_results'))
+        Answer.objects.create(question=question, user=self.user, answer="yes")
+        response = self.client.get(reverse("survey:survey_results"))
         self.assertContains(
             response,
-            '<td>Yes</td>',
+            "<td>Yes</td>",
             html=True,
         )
 
@@ -172,33 +209,31 @@ class SurveyFlowTests(TransactionTestCase):
         questions = self._create_questions(survey, 10)
         for idx, user in enumerate(self.users):
             self.client.logout()
-            self.client.login(username=user.username, password='pass')
-            data = {'question_id': questions[idx].pk, 'answer': 'yes'}
-            self.client.post(reverse('survey:answer_survey'), data)
+            self.client.login(username=user.username, password="pass")
+            data = {"question_id": questions[idx].pk, "answer": "yes"}
+            self.client.post(reverse("survey:answer_survey"), data)
             ans = Answer.objects.get(question=questions[idx], user=user)
-            self.assertEqual(ans.answer, 'yes')
+            self.assertEqual(ans.answer, "yes")
         self.assertEqual(Answer.objects.count(), 3)
 
     def test_cannot_answer_when_paused(self):
         survey = self._create_survey()
         question = self._create_question(survey)
-        survey.state = 'paused'
+        survey.state = "paused"
         survey.save()
 
-        response = self.client.get(reverse('survey:answer_survey'))
-        self.assertRedirects(response, reverse('survey:survey_detail'))
+        response = self.client.get(reverse("survey:answer_survey"))
+        self.assertRedirects(response, reverse("survey:survey_detail"))
 
-        data = {'question_id': question.pk, 'answer': 'yes'}
-        response = self.client.post(reverse('survey:answer_survey'), data)
+        data = {"question_id": question.pk, "answer": "yes"}
+        response = self.client.post(reverse("survey:answer_survey"), data)
         self.assertEqual(Answer.objects.count(), 0)
-        self.assertRedirects(response, reverse('survey:survey_detail'))
+        self.assertRedirects(response, reverse("survey:survey_detail"))
 
     def test_detail_shows_paused_alert(self):
         survey = self._create_survey()
-        survey.state = 'paused'
+        survey.state = "paused"
         survey.save()
 
-        response = self.client.get(reverse('survey:survey_detail'))
-        self.assertContains(response, 'This survey is currently paused.')
-
-
+        response = self.client.get(reverse("survey:survey_detail"))
+        self.assertContains(response, "This survey is currently paused.")
--- wikikysely_project/survey/urls.py
+++ wikikysely_project/survey/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
 from . import views
 
-app_name = 'survey'
+app_name = "survey"
 
 urlpatterns = [
-    path('', views.survey_detail, name='survey_detail'),
-    path('register/', views.register, name='register'),
-    path('survey/edit/', views.survey_edit, name='survey_edit'),
-    path('survey/answer/', views.answer_survey, name='answer_survey'),
-    path('survey/question/add/', views.question_add, name='question_add'),
-    path('question/<int:pk>/delete/', views.question_delete, name='question_delete'),
-    path('question/<int:pk>/restore/', views.question_restore, name='question_restore'),
-    path('question/<int:pk>/', views.answer_question, name='answer_question'),
-    path('answer/<int:pk>/edit/', views.answer_edit, name='answer_edit'),
-    path('answer/<int:pk>/delete/', views.answer_delete, name='answer_delete'),
-    path('answers/', views.answer_list, name='answer_list'),
-    path('results/', views.survey_results, name='survey_results'),
+    path("", views.survey_detail, name="survey_detail"),
+    path("register/", views.register, name="register"),
+    path("survey/edit/", views.survey_edit, name="survey_edit"),
+    path("survey/answer/", views.answer_survey, name="answer_survey"),
+    path("survey/question/add/", views.question_add, name="question_add"),
+    path("question/<int:pk>/edit/", views.question_edit, name="question_edit"),
+    path("question/<int:pk>/delete/", views.question_delete, name="question_delete"),
+    path("question/<int:pk>/restore/", views.question_restore, name="question_restore"),
+    path("question/<int:pk>/", views.answer_question, name="answer_question"),
+    path("answer/<int:pk>/edit/", views.answer_edit, name="answer_edit"),
+    path("answer/<int:pk>/delete/", views.answer_delete, name="answer_delete"),
+    path("answers/", views.answer_list, name="answer_list"),
+    path("results/", views.survey_results, name="survey_results"),
 ]
